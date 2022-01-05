7236b65252d72df7ba9917f372706723b1b714b1
1906olga <olga.ovcharenko1980@gmail.com>
2022-01-05 14:06:12 +0300

    new_file

---
+++ main.py
@@ -0,0 +1,10 @@
+# импортируем random module
+
+import random
+
+# выводит список который состоит
+# из атрибутов rundom function
+print('The contents of the library are::')
+
+# module Object используется как параметр
+print(dir(random))
